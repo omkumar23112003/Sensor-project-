ef39d78a5a02ef3bb6c5720dd4088cb908179c3e
omkumar23112003 <omk99919@gmail.com>
2025-07-31 23:20:39 +0530

    completed all modules

---
+++ srv/constant/__init__.py
@@ -0,0 +1,15 @@
+import os
+
+
+AWS_S3_BUCKET_NAME = "wafer-fault"
+MONGO_DATABASE_NAME = "omskills"
+MONGO_COLLECTION_NAME = "waferfault"
+
+
+TARGET_COLUMN = "quality"
+MONGO_DB_URL="mongodb+srv://omskills:om9971564935@cluster0.iiipw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+MODEL_FILE_NAME = "model"
+MODEL_FILE_EXTENSION = ".pkl"
+
+artifact_folder =  "artifacts"
